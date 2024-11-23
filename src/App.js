ade70f6a86ba5566cc14815c1f61efafbc22e91c
lankykiller <dani.mwai@gmail.com>
2024-11-24 00:43:09 +0200

    starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import { Footer, Blog, Possibility, Features, WhatGPT4, Header} from './containers'
-import {CTA, Brand, Navbar} from './components'
+import React from "react";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Feature,
+  WhatGPT4,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
 
 export const App = () => {
   return (
-    <div className='App'>
-      <div className='gradient_bg'>
-        <Navbar/>
-        <Navbar/>
-        <Navbar/>
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
       </div>
-      <Brand/>
-      <WhatGPT4/>
-      <Features/>
-      <Possibility/>
-      <CTA/>
-      <Blog/>
-      <Footer/>
+      <Brand />
+      <WhatGPT4 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
