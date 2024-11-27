645b009c6197e72c9df99d2da496feac400d01d4
lankykiller <dani.mwai@gmail.com>
2024-11-27 19:23:46 +0200

    som

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,15 @@ import './header.css';
 
 export const Header = () => {
   return (
-    <div>Header
+    <div className='gpt4__header section_padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Build with GPT-4</h1>
+        <div className='gpt4__header_content_input'>
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+        </div>
+      </div>
+      
     </div>
   )
 }
